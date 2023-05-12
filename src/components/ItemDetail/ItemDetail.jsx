c74112eddc2087fd204d825a72e2f5a301f23981
marcosmu96 <musellamarcos96@gmail.com>
2023-05-12 11:57:00 -0300

    listo detalles de la segunda entrega

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -4,16 +4,18 @@ import ItemCounter from "../ItemCounter/ItemCounter";
 
 export const ItemDetail = ({ product , onAdd}) => {
 
-return(
-  
+return( 
 <div className="detalles" key={product.id}>
-<h1>MARCA:{product.car_factory}</h1>
+<div className="loading">{!product.car_img && <section><span className="loader-93"> </span></section> }</div>
+
+{product.car_img && <div> <h1>MARCA:{product.car_factory}</h1>
 <h2>MODELO:{product.car_model}</h2>
 <img src={product.car_img} alt="Imagen del auto Destacado"></img>
 <h2>PRECIO:{product.Price}USD</h2>
 <h3>CANTIDAD A LA VENTA:{product.stock}</h3>
 <h3>AÑO:{product.car_year}</h3>
-<ItemCounter stock = {product.stock} onAdd={onAdd}/>
+<ItemCounter stock = {product.stock} onAdd={onAdd}/></div>}
+
 </div>
 
 )
